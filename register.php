2c0b6fea500cc00920d12aeda5cb8aa766c42dbe
JoacoRu <joacorubek@gmail.com>
2018-08-18 18:59:46 -0300

    Entrar

--- register.php
+++ register.php
@@ -27,6 +27,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv=”Last-Modified” content="0">
     <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
     <link rel="stylesheet" href="css/registrarse.css">
     <title>Random Choose Register</title>
@@ -42,7 +43,7 @@
 			<div class="div-errores alert alert-danger">
 				<ul>
 					<?php foreach ($errores as $value): ?>
-					<li style="font-family: 'Oswald', sans-serif;"><?=$value?></li>
+					<li style="font-family: 'Oswald', sans-serif;">-<?=$value?></li>
 					<?php endforeach; ?>
 				</ul>
 			</div>
